306c43bee2339c1d2e05c7d5e5c460bdc1f2f35a
Fritz Grabo <hello@fritzgrabo.com>
2025-11-04 18:16:58 +0100

    Add devcontainer configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://git-training.at',
+
+  server: {
+    host: true,
+    allowedHosts: true
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
